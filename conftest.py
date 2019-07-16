81f4c3a03971dfdb9d7338c4469cee5d7d1f0204
ushakovp <ushakovpb@gmail.com>
2019-07-16 10:49:30 +0400

    Change config to support Firefox

--- conftest.py
+++ conftest.py
@@ -1,10 +1,19 @@
 import pytest
 from selenium import webdriver
 
+def pytest_adoption(parser):
+    parser.adoption('--browser_name', action='store', default=None, help="Choose browser: Chrome or firefox")
+
 @pytest.fixture(scope='function')
-def browser():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('w3c', False)
-    browser = webdriver.Chrome(options=options)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('w3c', False)
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        print('Browser {} still is not implemented'.format(browser_name))
     yield browser
     browser.quit()
